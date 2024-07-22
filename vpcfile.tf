cb80c43306b5b7e15d54ebf1ad2b20be697bc1ca
VenkatVGS <venkat.tendulkar@gmail.com>
2024-07-22 19:09:22 +0530

    6th commit

--- vpcfile.tf
+++ vpcfile.tf
@@ -65,6 +65,7 @@ resource "aws_route_table" "pvt-route" {
 
   route {
     cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.ngw.id
     #vpc_peering_connection_id = aws_vpc_peering_connection.mypeer.id
   }
   tags = {
