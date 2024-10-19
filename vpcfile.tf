10e256dd4047c343e3a5fda918f945d15ca77582
VenkatVGS <159562946+VenkatVGS@users.noreply.github.com>
2024-10-19 23:50:34 +0530

    Update vpcfile.tf

--- vpcfile.tf
+++ vpcfile.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "ap-south-1"
-
-}
-
-
 # Create New VPC
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
