38ce9af1d348da12f926f4ae397e5f86b9b3d0ae
VenkatVGS <159562946+VenkatVGS@users.noreply.github.com>
2024-10-19 23:55:18 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+ # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "vgsterraform"
+    key    = "vgs/terraform.tfstate"
+    region = "ap-south-1"   
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
 
