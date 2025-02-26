34c10f18856916b07ed45b13ddccdc12434f4330
VenkatVGS <159562946+VenkatVGS@users.noreply.github.com>
2025-02-26 13:25:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # Create EC2 Instance
 resource "aws_instance" "my-ec2-vm" {
-  # ami               = "ami-00bb6a80f01f03502"  #Ubuntu
-  ami               = "ami-0d2614eafc1b0e4d2"  #windows
+  ami               = "ami-00bb6a80f01f03502"  #Ubuntu
+  #ami               = "ami-0d2614eafc1b0e4d2"  #windows
   instance_type     = "t2.micro"
   availability_zone = "ap-south-1b"
   #availability_zone = "ap-south-1a"
   tags = {
-    # "Name" = "web"
-    "tag1" = "Update-test-1"    
+     "Name" = "web"
+    #"tag1" = "Update-test-1"    
   }
 }
 
