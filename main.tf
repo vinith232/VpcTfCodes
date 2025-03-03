d724b0914b441356fd9dc29b59af19a34eebc24d
root <root@ip-172-31-16-32.ec2.internal>
2025-03-03 06:24:12 +0000

    New Instance

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Create EC2 Instance
 resource "aws_instance" "my-ec2-vm" {
-  ami               = "ami-00bb6a80f01f03502"  #Ubuntu
-  #ami               = "ami-0d2614eafc1b0e4d2"  #windows
+  #ami               = "ami-00bb6a80f01f03502"  #Ubuntu
+  ami               = "ami-0d2614eafc1b0e4d2"  #windows
   instance_type     = "t2.micro"
   availability_zone = "ap-south-1b"
   #availability_zone = "ap-south-1a"
