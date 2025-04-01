fc820c4aaeb25a421bf7f5c64f20a049959f57a5
Ajith <113841241+Ajith3114@users.noreply.github.com>
2025-04-01 18:24:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "my-ec2-vm" {
   ami               = "ami-00bb6a80f01f03502"  #Ubuntu
   #ami               = "ami-0d2614eafc1b0e4d2"  #windows
   instance_type     = "t2.micro"
-  availability_zone = "ap-south-1b"
-  #availability_zone = "ap-south-1a"
+  availability_zone = "eu-west-2a"
+  #availability_zone = "eu-west-2b"
   tags = {
      "Name" = "web"
     #"tag1" = "Update-test-1"    
