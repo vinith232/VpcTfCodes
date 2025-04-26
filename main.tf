66915d4da030e9c149028a29c1d099adeafa9647
vinith232 <92229301+vinith232@users.noreply.github.com>
2025-04-26 14:52:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my-ec2-vm" {
   ami               = "ami-01e479df1702f1d13"  #Ubuntu
   #ami               = "ami-0d2614eafc1b0e4d2"  #windows
   instance_type     = "t2.micro"
-  availability_zone = "eu-west-2a"
+  availability_zone = "ap-south-1a"
   #availability_zone = "eu-west-2b"
   tags = {
      "Name" = "web"
