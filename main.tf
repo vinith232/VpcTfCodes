c45141fa5edfcf82a00ce797141b9a442ebfc78f
vinith232 <92229301+vinith232@users.noreply.github.com>
2025-04-26 15:00:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Create EC2 Instance
 resource "aws_instance" "my-ec2-vm" {
-  ami               = "ami-01e479df1702f1d13"  #Ubuntu
+  ami               = "ami-06b6e5225d1db5f46"  #Ubuntu
   #ami               = "ami-0d2614eafc1b0e4d2"  #windows
   instance_type     = "t2.micro"
   availability_zone = "ap-south-1a"
