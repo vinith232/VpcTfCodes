7de270616462aedaebad1dbd6a5f8a36db5f68a4
vinith232 <92229301+vinith232@users.noreply.github.com>
2025-07-17 18:23:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Create EC2 Instance
 resource "aws_instance" "my-ec2-vm" {
-  count             = 2
+  count             = 3
   ami               = "ami-06b6e5225d1db5f46"  #Ubuntu
   #ami               = "ami-0d2614eafc1b0e4d2"  #windows
   instance_type     = "t2.micro"
